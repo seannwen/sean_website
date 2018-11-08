9d084293751d64a25144de42d19963c60fba08c5
tsunhsiang wen <wents@oregonstate.edu>
2018-11-07 22:20:46 -0800

    modified nav bar in mobile

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,49 +1,60 @@
-            // Get the modal
-            var modal1 = document.getElementById('myModal1');
-            var modal2 = document.getElementById('myModal2');
-            var modal3 = document.getElementById('myModal3');
+// Get the modal
+var modal1 = document.getElementById('myModal1');
+var modal2 = document.getElementById('myModal2');
+var modal3 = document.getElementById('myModal3');
 
-            // Get the button that opens the modal
-            var btn1 = document.getElementById("myBtn1");
-            var btn2 = document.getElementById("myBtn2");
-            var btn3 = document.getElementById("myBtn3");
+// Get the button that opens the modal
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
 
-            // Get the <span> element that closes the modal
-            var span1 = document.getElementsByClassName("close")[0];
-            var span2 = document.getElementsByClassName("close")[1];
-            var span3 = document.getElementsByClassName("close")[2];
+// Get the <span> element that closes the modal
+var span1 = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[1];
+var span3 = document.getElementsByClassName("close")[2];
 
-            // When the user clicks the button, open the modal 
-            btn1.onclick = function() {
-                modal1.style.display = "block";
-            };
-            btn2.onclick = function() {
-                modal2.style.display = "block";
-            };
-            btn3.onclick = function() {
-                modal3.style.display = "block";
-            };
+// When the user clicks the button, open the modal 
+btn1.onclick = function() {
+    modal1.style.display = "block";
+};
+btn2.onclick = function() {
+    modal2.style.display = "block";
+};
+btn3.onclick = function() {
+    modal3.style.display = "block";
+};
 
-            // When the user clicks on <span> (x), close the modal
-            span1.onclick = function() {
-                modal1.style.display = "none";
-            };
-            span2.onclick = function() {
-                modal2.style.display = "none";
-            };
-            span3.onclick = function() {
-                modal3.style.display = "none";
-            };
+// When the user clicks on <span> (x), close the modal
+span1.onclick = function() {
+    modal1.style.display = "none";
+};
+span2.onclick = function() {
+    modal2.style.display = "none";
+};
+span3.onclick = function() {
+    modal3.style.display = "none";
+};
 
-            // When the user clicks anywhere outside of the modal, close it
-            window.onclick = function(event) {
-                if (event.target == modal1) {
-                    modal1.style.display = "none";
-                }
-                if (event.target == modal2) {
-                    modal2.style.display = "none";
-                }
-                if (event.target == modal3) {
-                    modal3.style.display = "none";
-                }
-            };
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal1) {
+        modal1.style.display = "none";
+    }
+    if (event.target == modal2) {
+        modal2.style.display = "none";
+    }
+    if (event.target == modal3) {
+        modal3.style.display = "none";
+    }
+};
+
+
+// dropdownIcon
+function dropdownMenu() {
+    var x = document.getElementById('dropList');
+    if (x.className === "container") {
+        x.className === " responsive";
+    } else {
+        x.className = "container";
+    }
+}
